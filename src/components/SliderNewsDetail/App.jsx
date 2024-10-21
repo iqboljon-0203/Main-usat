2680e3ded74a5dd55f4096a90a1d8c11f5420503
Iqboljon <iqboljon2003.yuldashev@gmail.com>
2024-10-21 17:46:02 +0500

    Finished items  pages responsive

--- src/components/SliderNewsDetail/App.jsx
+++ src/components/SliderNewsDetail/App.jsx
@@ -7,7 +7,7 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/navigation';
 import 'swiper/css/thumbs';
-
+import { useEffect } from 'react';
 import './App.css';
 import { useSelector } from 'react-redux';
 // import required modules
@@ -16,16 +16,66 @@ import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 export default function App() {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
     const { newsItem} = useSelector((state) => state.getNews)
-    console.log(newsItem);
-    
+    const [swiperHeight, setSwiperHeight] = useState(650);
+    const [littleSwiperHeight,setLittleSwiperHeight]=useState(188)
+     useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 480) {
+        setSwiperHeight(180);
+      } else if (window.innerWidth <= 768) {
+        setSwiperHeight(250);
+      }else if (window.innerWidth <= 1000) {
+        setSwiperHeight(350);
+      } else if (window.innerWidth <= 1500) {
+        setSwiperHeight(450);
+      } else {
+        setSwiperHeight(650);
+      }
+    };
+
+    // Call handler once to set initial size
+    handleResize();
+
+    // Add event listener to update height on resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 480) {
+        setLittleSwiperHeight(100);
+      } else if (window.innerWidth <= 768) {
+        setLittleSwiperHeight(130);
+      }else if (window.innerWidth <= 1000) {
+        setLittleSwiperHeight(150);
+      } else if (window.innerWidth <= 1500) {
+        setLittleSwiperHeight(170);
+      } else {
+        setLittleSwiperHeight(188);
+      }
+    };
+
+    // Call handler once to set initial size
+    handleResize();
+
+    // Add event listener to update height on resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
       <Swiper
         style={{
           '--swiper-navigation-color': '#fff',
           '--swiper-pagination-color': '#fff',
-          height: '650px',
+          height: `${swiperHeight}px`,
         }}
+       
         loop={true}
         spaceBetween={10}
         navigation={true}
@@ -35,7 +85,7 @@ export default function App() {
       >
         {newsItem&&newsItem?.photos?.map((item) => (
             <SwiperSlide>
-                <img src={item} alt="News info image" />
+                <img style={{height:"100%"}} src={item} alt="News info image" />
             </SwiperSlide>
         ))}
       </Swiper>
@@ -48,11 +98,11 @@ export default function App() {
         watchSlidesProgress={true}
         modules={[FreeMode, Navigation, Thumbs]}
         className="myNewsSwiper"
-        style={{height: '188px'}}
+        style={{height: `${littleSwiperHeight}px`}}
       >
         {newsItem&&newsItem?.photos?.map((item) => (
             <SwiperSlide>
-                <img style={{height: '100%',objectFit:"cover"}} src={item} alt="News info image" />
+                <img style={{height:"100%"}} src={item} alt="News info image" />
             </SwiperSlide>
         ))}
       </Swiper>
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -28,80 +28,32 @@ const NewsPage = () => {
     }, [dispatch, name,languageNew])
     const { newsItem} = useSelector((state) => state.getNews)
 
-      const customStyle = {
-    width: '100%',
-    fontFamily: 'Inter, sans-serif',
-    fontSize: '24px',
-    fontWeight: 400,
-    lineHeight: '33.6px',
-    textAlign: 'left',
-    color: '#21466d',
-    marginBottom:"30px",
-    marginTop:"30px",
-  };
+     
   return (
       <div className="newsPage">
           <Navbar child="true"></Navbar>
           <p  className="newsPage_path">
         <Link to="/">{t("sahifa")} </Link>  <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '20px',
-        height: '20px', // Butun ekran bo'yi o'lchov uchun
-        margin:"0px 10px",
-      }}
+     className='dot_parent'
     >
       <div
-        style={{
-          width: '6px',
-          height: '6px',
-          backgroundColor: '#21466D',
-          borderRadius: '50%',
-          
-        }}
+        className='dot_child'
       ></div>
     </div><span><Link to={'/news'}>Yangiliklar</Link></span>  <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '20px',
-        height: '20px', // Butun ekran bo'yi o'lchov uchun
-        margin:"0px 10px",
-      }}
+      className='dot_parent'
     >
       <div
-        style={{
-          width: '6px',
-          height: '6px',
-          backgroundColor: '#21466D',
-          borderRadius: '50%',
-        }}
+        className='dot_child'
       ></div>
     </div>{" "}
         <span><Link to={`/news/${newsItem && newsItem?.category?.slug}`}>{newsItem && newsItem?.category?.title}</Link></span>
-         <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '20px',
-        height: '20px', // Butun ekran bo'yi o'lchov uchun
-        margin:"0px 10px",
-      }}
+         <span
+      className='dot_parent'
     >
-      <div
-        style={{
-          width: '6px',
-          height: '6px',
-          backgroundColor: '#21466D',
-          borderRadius: '50%',
-          
-        }}
-      ></div>
-    </div>{" "}
+      <span
+        className='dot_child'
+      ></span>
+    </span>{" "}
     <span><Link to={`/news/${newsItem && newsItem?.category?.slug}/${newsItem&&newsItem?.slug}`}>{newsItem && newsItem.title}</Link></span>
       </p>
           <h2 className="newsPage_title">{newsItem && newsItem.title}</h2>
@@ -120,9 +72,9 @@ const NewsPage = () => {
                   src={newsItem && newsItem.photo}
                   alt="News image"
               />
-              <div style={customStyle} dangerouslySetInnerHTML={{ __html: newsItem && newsItem.content }} />
+              <div className='newsPage_content_text'  dangerouslySetInnerHTML={{ __html: newsItem && newsItem.content }} />
                <SliderNewsDetail></SliderNewsDetail>
-               <div style={customStyle} dangerouslySetInnerHTML={{ __html: newsItem && newsItem.content2 }} />
+               <div className='newsPage_content_text'  dangerouslySetInnerHTML={{ __html: newsItem && newsItem.content2 }} />
                <div className='newsPage_content_buttons'>
                     <div className='newsPage_content_buttons_left'>
                         <button><img src={ShareIcon} alt="Share icon" />Ulashish</button>
